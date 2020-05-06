528e587867e7fb062e2468dd7a818c81ec8cb572
Prashant Agrawal <prashant.agrawal@hashedin.com>
2020-05-06 16:32:24 +0530

    TimeSheetApp

---
+++ src/components/TaskList/index.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import './styles.scss';
+import { IEntry } from '../NewEntrySheet';
+
+interface ITaskListProps {
+    entries: IEntry[];
+}
+interface ITaskCardProps {
+    entry: IEntry;
+}
+
+export const TaskList: React.FC<ITaskListProps> = (props: ITaskListProps) => {
+    const { entries } = props;
+    return (
+        <div className="task-list">
+            {entries.map((entry: IEntry) => (
+                <TaskCard entry={entry} />
+            ))}
+        </div>
+    );
+};
+
+const TaskCard: React.FC<ITaskCardProps> = (props: ITaskCardProps) => {
+    const {
+        entry: { task, hours, minutes },
+    } = props;
+    return (
+        <div className="task-card">
+            <div className="task-title">{task}</div>
+            <div className="task-time">{`${hours}h ${minutes}m`}</div>
+        </div>
+    );
+};
